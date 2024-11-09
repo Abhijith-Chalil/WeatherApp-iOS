dcc623ae40927b96f32959ff683503e90892d631
Abhijith C <abhijithchalilabc@gmail.com>
2024-11-09 22:30:50 +0530

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Abhijith Chalil on 09/11/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
